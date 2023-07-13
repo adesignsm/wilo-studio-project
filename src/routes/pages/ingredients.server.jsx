2ac51c01093d096ecf9e5f51eda232278ded21ae
jnettek <jnettekpelletier@live.com>
2023-07-13 15:32:08 -0400

    FloatingStrings.client.jsx float strings attemp

--- src/routes/pages/ingredients.server.jsx
+++ src/routes/pages/ingredients.server.jsx
@@ -1,11 +1,15 @@
 import Layout from "../../components/Layout.server";
+// import FloatingStrings from "../../components/FloatingStrings.client";
 import "../../styles/ingredients.css";
 
 const Ingredients = () => {
+
+    
     return (
         <>
             <Layout>
                 <h1> Ingredients TEST </h1>
+                {/* <FloatingStrings/> */}
             </Layout>
         </>
     )

9a163a6622ce2f23f582a56a8aa2fe6edd263f36
Akash Mulye <akashmulye97@gmail.com>
2023-08-29 21:04:26 -0400

    added back the config for now

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -3,9 +3,12 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: import.meta.env.VITE_STORE_DOMAIN || 'default.stud',
-    storefrontToken: import.meta.env.VITE_STOREFRONT_TOKEN || 'default_token',
-    storefrontApiVersion: import.meta.env.VITE_STOREFRONT_API_VERSION || 'default_version',
+    storeDomain: 'wilo.studio',
+    storefrontToken: '3619e2a77120857c6e57862b096710b6',
+    storefrontApiVersion: '2022-07',
+    // storeDomain: import.meta.env.VITE_STORE_DOMAIN || 'default.stud',
+    // storefrontToken: import.meta.env.VITE_STOREFRONT_TOKEN || 'default_token',
+    // storefrontApiVersion: import.meta.env.VITE_STOREFRONT_API_VERSION || 'default_version',
   },
   server: {
     logger: {

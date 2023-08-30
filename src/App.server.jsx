9572c75a22d7a47f5ea2874ccf3dcf62585ad548
Akash Mulye <akashmulye97@gmail.com>
2023-08-30 12:04:22 -0400

    style fix 7

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -2,6 +2,15 @@ import renderHydrogen from '@shopify/hydrogen/entry-server';
 import {Router, FileRoutes, ShopifyProvider, CartProvider} from '@shopify/hydrogen';
 import {Suspense} from 'react';
 
+import "./styles/index.css";
+import "./styles/cartPage.css";
+import "./styles/productDetails.css";
+import "./styles/ingredients.css";
+import "./styles/policy.css";
+import "./styles/shopAll.css";
+import "./styles/skin.css";
+import "./styles/homeGoods.css";
+
 function App() {
   return (
     <Suspense fallback={null}>

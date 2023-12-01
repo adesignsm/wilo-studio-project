0a7dd3cfd1b392b41b0ed202d13aa58244900e0e
Akash Mulye <akashmulye97@gmail.com>
2023-12-01 13:34:00 -0500

    removed accidental main branch. Causing breaking issues

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,19 +2,16 @@ import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    defaultCountryCode: '',
-    defaultLanguageCode: '',
-    storeDomain: Oxygen.env.PUBLIC_STORE_DOMAIN,
-    storefrontToken: Oxygen.env.PUBLIC_STOREFRONT_API_TOKEN,
-    privateStorefrontToken: Oxygen.env.PRIVATE_STOREFRONT_API_TOKEN,
+    storeDomain: 'wilo-candles.myshopify.com',
+    storefrontToken: '3619e2a77120857c6e57862b096710b6',
     storefrontApiVersion: '2022-07',
-    storefrontId: Oxygen.env.PUBLIC_STOREFRONT_ID,
+    // storeDomain: import.meta.env.VITE_STORE_DOMAIN || 'default.stud',
+    // storefrontToken: import.meta.env.VITE_STOREFRONT_TOKEN || 'default_token',
+    // storefrontApiVersion: import.meta.env.VITE_STOREFRONT_API_VERSION || 'default_version',
   },
-  session: CookieSessionStorage('__session', {
-    path: '/',
-    httpOnly: true,
-    secure: import.meta.env.PROD,
-    sameSite: 'Strict',
-    maxAge: 60 * 60 * 24 * 30,
-  }),
+  server: {
+    logger: {
+      showQueryTiming: import.meta.env.VITE_SHOW_QUERY_TIMING === 'true',
+    },
+  },
 });

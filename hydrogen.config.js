d9f89333e520ee2ec5b46a356b08f8bf567125e0
Akash Mulye <akashmulye97@gmail.com>
2023-12-01 14:26:11 -0500

    remvoed file

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -5,9 +5,6 @@ export default defineConfig({
     storeDomain: 'wilo-candles.myshopify.com',
     storefrontToken: '3619e2a77120857c6e57862b096710b6',
     storefrontApiVersion: '2022-07',
-    // storeDomain: import.meta.env.VITE_STORE_DOMAIN || 'default.stud',
-    // storefrontToken: import.meta.env.VITE_STOREFRONT_TOKEN || 'default_token',
-    // storefrontApiVersion: import.meta.env.VITE_STOREFRONT_API_VERSION || 'default_version',
   },
   server: {
     logger: {

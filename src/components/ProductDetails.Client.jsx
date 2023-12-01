d122c68f78a94cb1c190e6fab433f4c258d4d334
Akash Mulye <akashmulye97@gmail.com>
2023-12-01 15:54:34 -0500

    fixing

--- src/components/ProductDetails.Client.jsx
+++ src/components/ProductDetails.Client.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import { 
   useProductOptions,
   AddToCartButton, 
   ProductOptionsProvider,
   ProductPrice,
   CartLineProvider,
+  CartProvider,
   useCart,
   CartLineQuantityAdjustButton,
   CartLineQuantity,
@@ -40,70 +41,70 @@ import {
 
     return (
       <>
-        <div className="product-container">
-          <div className="product-name">
-            <h1>{product.title}</h1>
-            <div className="product-details">
-              <p>Info</p>
-              <p>{product.description}</p>
-            </div>
-            {product.tags.length > 0 && 
-              <div className="ingredients-details">
-                <p>Ingredients</p>
-                <div className="ingredients-list">
-                  {product.tags.map((ingredient, index) => (
-                    <span key={index}>
-                      <Link to={`ingredients/${ingredient.toLowerCase().replace(" ", "-")}`} className="tag-link">
-                        {ingredient}
-                      </Link>
-                      {index < product.tags.length - 1 && <span>, </span>}
-                    </span>
-                  ))}
-                </div>
+          <div className="product-container">
+            <div className="product-name">
+              <h1>{product.title}</h1>
+              <div className="product-details">
+                <p>Info</p>
+                <p>{product.description}</p>
               </div>
-            }
-            <ProductPrice className="product-page-price" withoutTrailingZeros data={product} variantId={selectedVariant.id} />
-            <div className="add-to-cart-container">
-                <div className="add-to-cart-box">
-                  <AddToCartButton disabled={isOutOfStock} className="add-to-cart" >
-                    {isOutOfStock ? 'Out of stock' : 'Add to cart'}
-                   </AddToCartButton>
+              {product.tags.length > 0 && 
+                <div className="ingredients-details">
+                  <p>Ingredients</p>
+                  <div className="ingredients-list">
+                    {product.tags.map((ingredient, index) => (
+                      <span key={index}>
+                        <Link to={`ingredients/${ingredient.toLowerCase().replace(" ", "-")}`} className="tag-link">
+                          {ingredient}
+                        </Link>
+                        {index < product.tags.length - 1 && <span>, </span>}
+                      </span>
+                    ))}
+                  </div>
                 </div>
-                {/* <div className="qty-box">
-                  {firstLine && 
-                    <CartLineProvider key={firstLine?.id || 'defaultKey'} line={firstLine || 0}>
-                      <CartLineQuantityAdjust />
-                    </CartLineProvider>
-                  }
-                </div> */}
+              }
+              <ProductPrice className="product-page-price" withoutTrailingZeros data={product} variantId={selectedVariant.id} />
+              <div className="add-to-cart-container">
+                  <div className="add-to-cart-box">
+                    <AddToCartButton disabled={isOutOfStock} className="add-to-cart" >
+                      {isOutOfStock ? 'Out of stock' : 'Add to cart'}
+                    </AddToCartButton>
+                  </div>
+                  {/* <div className="qty-box">
+                    {firstLine && 
+                      <CartLineProvider key={firstLine?.id || 'defaultKey'} line={firstLine || 0}>
+                        <CartLineQuantityAdjust />
+                      </CartLineProvider>
+                    }
+                  </div> */}
+              </div>
+            </div>
+    
+            <div className="product-media">
+              {product.images && product.images.edges && (
+                <ul>
+                  {product.images.edges.map(({ node }) => (
+                    <li key={node.url}>
+                      <img src={node.url} alt="" width={node.width} height={node.height} />
+                    </li>
+                  ))}
+                  {product.media && product.media.edges && (
+                    product.media.edges.map(({ node }) => {
+                      if (node.mediaContentType === "VIDEO") {
+                        return (
+                          <li key={node.sources[0].url}>
+                            <video autoPlay={toggleAutoPlay} muted={true} loop={true} playsInline={true}>
+                              <source src={node.sources[0].url} type="video/mp4"></source>
+                            </video>
+                          </li>
+                        )
+                      }
+                    })
+                  )}
+                </ul>
+              )}
             </div>
           </div>
-  
-          <div className="product-media">
-            {product.images && product.images.edges && (
-              <ul>
-                {product.images.edges.map(({ node }) => (
-                  <li key={node.url}>
-                    <img src={node.url} alt="" width={node.width} height={node.height} />
-                  </li>
-                ))}
-                {product.media && product.media.edges && (
-                  product.media.edges.map(({ node }) => {
-                    if (node.mediaContentType === "VIDEO") {
-                      return (
-                        <li key={node.sources[0].url}>
-                          <video autoPlay={toggleAutoPlay} muted={true} loop={true} playsInline={true}>
-                            <source src={node.sources[0].url} type="video/mp4"></source>
-                          </video>
-                        </li>
-                      )
-                    }
-                  })
-                )}
-              </ul>
-            )}
-          </div>
-        </div>
       </>
     );
   };
